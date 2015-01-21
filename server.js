4a402ee4f78c535b2baeef4c96136e5339fd25f5
Rodrigo Perez <rodrigo.perez@yale.edu>
2015-01-21 05:38:42 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Nice!');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('cooperative lemur');
 });
 
 app.listen(process.env.PORT || 4000);
